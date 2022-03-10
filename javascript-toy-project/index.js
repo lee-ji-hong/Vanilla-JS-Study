dc052d73426e9a415818c428f0166811dc20e6cf
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2022-03-10 16:39:14 +0900

    Feat : " input, button 이벤트 추가"

--- javascript-toy-project/index.js
+++ javascript-toy-project/index.js
@@ -7,3 +7,16 @@
 // [] 올바를 경우 c는 B가 말할 단어의 끝음절로 시작하는 단어를 말한다.
 // [] 올바르지 않을 경우에는 실패라고 표시한다.
 // [] 게임을 처음부터 다시 진행한다.
+
+const number = Number(prompt("몇 명1이 참가하나요?"));
+
+// 입력 버튼 클릭 이벤트
+const onClickButton = () => {};
+const $button = document.querySelector(".btn-ghost");
+
+// input에 글자 입력 이벤트
+const onInput = (e) => {};
+const $input = document.querySelector(".main-input");
+
+$button.addEventListener("click", onClickButton);
+$input.addEventListener("input", onInput);
